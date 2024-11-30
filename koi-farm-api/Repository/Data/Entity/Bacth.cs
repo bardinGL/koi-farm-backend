9128d78fe2f5a05d0f3a00657839f503dc1a29a7
MinhThong <phamminhthong582@gmail.com>
2024-11-30 22:57:45 +0700

    commit

---
+++ koi-farm-api/Repository/Data/Entity/Bacth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Entity
+{
+    [Table("Batch")]
+    public class Batch : Entity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<ProductItem> batchItems { get; set; }
+    }
+}
