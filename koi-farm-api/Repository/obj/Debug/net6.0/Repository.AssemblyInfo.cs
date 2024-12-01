6b895617c7e01a49980f39d40a4f1a2adbf560ec
MinhThong <phamminhthong582@gmail.com>
2024-12-01 21:15:04 +0700

    commit certificate

--- koi-farm-api/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
+++ koi-farm-api/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e4747843db28f106ea2b79a80fdb057c5bcb96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b0e3443c1af0ec52fc04edf80bd9f9d4b0b4f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
