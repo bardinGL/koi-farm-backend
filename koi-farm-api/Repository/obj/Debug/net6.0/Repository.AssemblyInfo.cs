1caf909c6cd5e91787a63fefee4634016ae50564
MinhThong09 <phamminhthong582@gmail.com>
2024-12-08 12:48:55 +0700

    coommit email

--- koi-farm-api/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
+++ koi-farm-api/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7722a2cb04e82a2bf8dded445cb5ead8453cb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc36b540393e876e91415ebadbdd851faa386429")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
