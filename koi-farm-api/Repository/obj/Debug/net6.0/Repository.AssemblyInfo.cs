a2eb81559452b370008162f1bacbbf9c6434da13
bardinGL <110034163+bardinGL@users.noreply.github.com>
2024-12-08 22:40:10 +0700

    Update

--- koi-farm-api/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
+++ koi-farm-api/Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30228fd402e476d5afd8a0d9f06543ab65a27cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bcbadc11422265c8e070ae4e29395e071a2ac3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
